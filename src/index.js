7eafcbdf2524542135c294a6985c9cdf01e56d86
Kevin Grandon <kevingrandon@yahoo.com>
2017-11-28 17:04:59 -0800

    Remove need for hooks into ctx during initial render
    Timing is a bit weird it seems, but everything works. Still need to remove the extra plugin.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-env browser */
 import CoreApp from 'fusion-core';
 import {prepare} from 'fusion-react-async';
+import {html} from 'fusion-core';
 import serverRender from './server';
 import clientRender from './client';
 
@@ -10,16 +11,16 @@ import Provider from './provider';
 
 export default class App extends CoreApp {
   constructor(root, getClient) {
-    super(null, (el, ctx) => {
+    super(null, el => {
       return prepare(el).then(() => {
-        return __NODE__ ? serverRender(el, ctx) : clientRender(el);
+        return __NODE__ ? serverRender(el) : clientRender(el);
       });
     });
 
     // This is required to set apollo client/root on context before creating the client.
     const preRenderPlugin = (ctx, next) => {
-      ctx.apolloClient = getClient(ctx);
       ctx.apolloRoot = root;
+      ctx.apolloClient = getClient(ctx);
       return next();
     };
 
@@ -32,7 +33,16 @@ export default class App extends CoreApp {
 function ApolloSSR() {
   return function middleware(ctx, next) {
     ctx.element = ctx.apolloRoot({client: ctx.apolloClient});
-    return next();
+    if (__NODE__) {
+      return next().then(() => {
+        const initialState = ctx.apolloClient.cache.extract();
+        const serialized = JSON.stringify(initialState);
+        const script = html`<script type="application/json" id="__APOLLO_STATE__">${serialized}</script>`;
+        ctx.body.body.push(script);
+      });
+    } else {
+      return next();
+    }
   };
 }
 
--- src/server.js
+++ src/server.js
@@ -1,13 +1,8 @@
 /* eslint-env node */
-import {html} from 'fusion-core';
 import {renderToStringWithData} from 'react-apollo';
 
-export default (root, ctx) => {
+export default root => {
   return renderToStringWithData(root).then(content => {
-    const initialState = ctx.apolloClient.cache.extract();
-    const serialized = JSON.stringify(initialState);
-    const script = html`<script type="application/json" id="__APOLLO_STATE__">${serialized}</script>`;
-    ctx.body.body.push(script);
     return `<div id='root'>${content}</div>`;
   });
 };
