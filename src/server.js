154db649cc862d9b55e022ab0130e33d90909d9f
Kevin Grandon <kevingrandon@yahoo.com>
2017-12-12 17:54:27 -0800

    Add initial flowtypes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
+// @flow
 /* eslint-env node */
 import {renderToStringWithData} from 'react-apollo';
 
-export default root => {
+export default (root: React$Element<any>) => {
   return renderToStringWithData(root).then(content => {
     return `<div id='root'>${content}</div>`;
   });
